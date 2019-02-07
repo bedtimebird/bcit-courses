753d549ea3589773fb2a6e0c78ec88a2e81b560c
bedtimebird <michaelbirdcet@gmail.com>
2019-02-07 14:28:29 -0800

    Update DateTest.java

--- comp1451/assignment_1/DateTest.java
+++ comp1451/assignment_1/DateTest.java
@@ -10,7 +10,7 @@ class DateTest {
   private int yearTest;
 
   @BeforeEach
-  void setUp() throws Exception {
+  public void setUp() throws Exception {
     dateTest = new Date(9,9,2014);
     dayTest = 9;
     monthTest = 9;
@@ -18,25 +18,119 @@ class DateTest {
   }
 
   @AfterEach
-  void tearDown() throws Exception {
+  public void tearDown() throws Exception {
     dateTest = null;
   }
 
   @Test
-  void testGetDay() {
-    assertEquals(09, dateTest.getDay());
+  public void testGetDay() {
+    assertEquals(9, dateTest.getDay());
   }
 
   @Test
-  void testGetDayNegative() {
-    dateTest.setDay(0);
-    assertEquals(01, dateTest.getDay());
+  public void testSetDayNegative() {
+    dateTest.setDay(-1);
+    assertEquals(1, dateTest.getDay());
   }
 
   @Test
-  void testGetDayBoundary() {
+  public void testSetDayLowerBounds() {
+    dateTest.setDay(1);
+    assertEquals(1, dateTest.getDay());
+  }
+
+  @Test
+  public void testSetDayUpperBounds() {
     dateTest.setDay(31);
     assertEquals(31, dateTest.getDay());
+  }
+
+  @Test
+  public void testSetDayOverAllowed() {
+    dateTest.setDay(32);
+    assertEquals(1, dateTest.getDay());
+  }  
+
+  @Test
+  public void testSetDayUnderAllowed() {
+    dateTest.setDay(0);
+    assertEquals(1, dateTest.getDay());
+  }  
+  
+  @Test
+  public void testGetMonth() {
+    assertEquals(9, dateTest.getMonth());
+  }
+
+  @Test
+  public void testSetMonthNegative() {
+    dateTest.setMonth(-1);
+    assertEquals(1, dateTest.getMonth());
+  }
+
+  @Test
+  public void testSetMonthLowerBounds() {
+    dateTest.setMonth(1);
+    assertEquals(1, dateTest.getMonth());
+  }
+
+  @Test
+  public void testSetMonthUpperBounds() {
+    dateTest.setMonth(12);
+    assertEquals(12, dateTest.getMonth());
+  }
+
+  @Test
+  public void testSetMonthOverAllowed() {
+    dateTest.setMonth(13);
+    assertEquals(1, dateTest.getMonth());
+  }  
+
+  @Test
+  public void testSetMonthUnderAllowed() {
+    dateTest.setMonth(0);
+    assertEquals(1, dateTest.getMonth());
+  }    
+  
+  @Test
+  public void testGetYear() {
+    assertEquals(2014, dateTest.getYear());
+  }
+
+  @Test
+  public void testSetYearNegative() {
+    dateTest.setMonth(-1);
+    assertEquals(1900, dateTest.getYear());
+  }
+
+  @Test
+  public void testSetYearLowerBounds() {
+    dateTest.setYear(1900);
+    assertEquals(1900, dateTest.getYear());
+  }
+
+  @Test
+  public void testSetYearUpperBounds() {
+    dateTest.setMonth(2019);
+    assertEquals(2019, dateTest.getYear());
+  }
+
+  @Test
+  public void testSetYearOverAllowed() {
+    dateTest.setYear(2020);
+    assertEquals(1900, dateTest.getYear());
+  }  
+
+  @Test
+  public void testSetYearUnderAllowed() {
+    dateTest.setYear(1899);
+    assertEquals(1900, dateTest.getYear());
+  }      
+
+  @Test
+  public void testCheckDateInputDayWrongMax() {
+    dateTest.checkDateInput(22, 1, 65);
+    assertEquals(22, dateTest.getDay());
   }
 
   
