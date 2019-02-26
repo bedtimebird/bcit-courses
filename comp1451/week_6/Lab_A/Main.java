5481cfe5af3d15c235574a5b44da5ab64f7ece8e
bedtimebird <michaelbirdcet@gmail.com>
2019-02-26 10:07:28 -0800

    Create Main.java

---
+++ comp1451/week_6/Lab_A/Main.java
@@ -0,0 +1,10 @@
+/**
+* @author A00964363
+*/
+public class Main {
+	public static void main(String[] args) {
+    PersonCollection pc1 = new PersonCollection();
+    pc1.loadCollection();
+    pc1.displayAllDetails();
+	}
+}
