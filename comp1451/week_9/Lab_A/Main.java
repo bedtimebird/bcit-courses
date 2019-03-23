a690c877c8a2c55d2cba067868be2292c086e716
bedtimebird <michaelbirdcet@gmail.com>
2019-03-22 19:19:03 -0700

    Create Main.java

---
+++ comp1451/week_9/Lab_A/Main.java
@@ -0,0 +1,22 @@
+package ca.bcit.comp1451.Session9LabA;
+
+/**
+ * 
+ */
+
+/**
+ * @author Rana
+ *
+ */
+public class Main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		Picture p = new Picture();
+		p.draw();
+
+	}
+
+}
