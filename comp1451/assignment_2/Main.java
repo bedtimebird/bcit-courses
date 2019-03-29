c208f542a06b518e6a740a0da8f157b755f979cd
bedtimebird <michaelbirdcet@gmail.com>
2019-03-29 12:06:41 -0700

    Update Main.java

--- comp1451/assignment_2/Main.java
+++ comp1451/assignment_2/Main.java
@@ -1,12 +1,22 @@
 class Main {
   public static void main(String[] args) {
-    Labour lab1 = new Labour("first labour project", 50.0, 20.0, 15.00, "regular", "experienced");
-    System.out.println(lab1.toString());
-    System.out.println("------");
-    LabourAndMaterial lab2 = new LabourAndMaterial("second labour and material project", 80.0, 20.0, 20.00, "regular", "experienced", 575.0, 5.0, 16.0);
-    System.out.println(lab2.toString());
-    System.out.println("------");
-    LabourAndMaterialAndEquipment lab3 = new LabourAndMaterialAndEquipment("second labour and material project", 80.0, 20.0, 20.00, "regular", "inexperienced", 575.0, 5.0, 16.0, 5000, 20);
-    System.out.println(lab3.toString());
-  }
+    InsuranceCompany ing = new InsuranceCompany("ING");
+    ing.addInvoice(new Labour("Large Hadron Collider", 5000.0, 125.0, 1500.00, "regular", "experienced"));
+    ing.addInvoice(new Labour("Mars’ Opportunity and Curiosity rovers", 2500.0, 20.0, 15000.00, "regular", "experienced"));
+    ing.addInvoice(new LabourAndMaterial("Three Gorges Dam", 10000.0, 10.0, 200.00, "overtime", "inexperienced", 5.0, 210000.0, 35.0));
+    ing.addInvoice(new LabourAndMaterial("Burj Khalifa", 2000.0, 25.0, 50.00, "holiday", "inexperienced", 1000.0, 500.0, 50.0));
+    ing.addInvoice(new LabourAndMaterialAndEquipment("Brenner Base Tunnel", 1500.0, 100.0, 50.00, "regular", "experienced", 200.0, 50000.0, 50.0, 500000, 200));
+    ing.addInvoice(new LabourAndMaterialAndEquipment("Millau Viaduct", 1200.0, 300.0, 10.00, "regular", "experienced", 2000.0, 4000.0, 15.0, 5000, 120));
+    ing.displayAllInvoices();
+    ing.calcTotalInsuranceFees();
+    
+/**
+    try {
+      ing.displayInvoice("INVC00001");
+    } catch(InvalidInvoiceNumberException e){
+      System.out.println(e.getMessage());
+    }
+    ing.displayAllInvoices();
+**/
+  } 
 }
