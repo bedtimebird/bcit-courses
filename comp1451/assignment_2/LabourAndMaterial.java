1cae6b4c5da13ec273c0c4080fbaa92027d580cd
bedtimebird <michaelbirdcet@gmail.com>
2019-04-05 14:04:16 -0700

    Update LabourAndMaterial.java

--- comp1451/assignment_2/LabourAndMaterial.java
+++ comp1451/assignment_2/LabourAndMaterial.java
@@ -1,3 +1,7 @@
+/**
+ * @author A00964363
+ */
+
 public class LabourAndMaterial extends Labour implements Transferable{
 
   private static final double MARK_UP = 1.15;
@@ -6,36 +10,67 @@ public class LabourAndMaterial extends Labour implements Transferable{
   private double matPurchPrice; //material purchase price
   private double matVolCubicFT; //material volume in cubic feet
   private double matTransportDisKM; //transportation distance for material in KM
-
+  /**
+  * @constructor
+  */
   public LabourAndMaterial(String projectName, double hoursWorked, double hourlyRate, double transportDistKM, String hourlyRateCriteria, String typeOfLabour, double matPurchPrice, double matVolCubicFT, double matTransportDisKM){
     super(projectName, hoursWorked, hourlyRate, transportDistKM, hourlyRateCriteria, typeOfLabour);
     setMatPurchPrice(matPurchPrice);
     setMatVolCubicFT(matVolCubicFT);
     setMatTransportDisKM(matTransportDisKM);
   }
+  /**
+  * apply Checks doubleValue())
+  * @param matPurchPrice double
+  */  
   public void setMatPurchPrice(double matPurchPrice){
     this.matPurchPrice = Checks.doubleValue(matPurchPrice);
   }
+  /**
+  * @return double matPurchPrice
+  */    
   public double getMatPurchPrice(){
     return matPurchPrice;
   }
+  /**
+  * apply Checks doubleValue())
+  * @param matVolCubicFT double
+  */    
   public void setMatVolCubicFT(double matVolCubicFT){
     this.matVolCubicFT = Checks.doubleValue(matVolCubicFT);
   }
+  /**
+  * @return double matVolCubicFT
+  */      
   public double getMatVolCubicFT(){
     return matVolCubicFT;
   }
+  /**
+  * apply Checks doubleValue())
+  * @param matTransportDisKM double
+  */      
   public void setMatTransportDisKM(double matTransportDisKM){
     this.matTransportDisKM = Checks.doubleValue(matTransportDisKM);
   }
+  /**
+  * @return double matTransportDisKM
+  */     
   public double getMatTransportDisKM(){
     return matTransportDisKM;
   }
-  public double matertialTotalCost(){
-    double matertialTotalCost = getMatPurchPrice() * MARK_UP;
-    return matertialTotalCost;
+  /**
+  * method to calculate value of material Cost
+  * @return materialTotalCost double
+  */  
+  public double materialTotalCost(){
+    double materialTotalCost = getMatPurchPrice() * MARK_UP;
+    return materialTotalCost;
   }
-
+  /**
+  * @Override method to calculate transportation cost
+  * apply if statement to calculate value based on distance and volume
+  * @return materialTransportCost double
+  */
   @Override
   public double calcTransportationCost(){
     double materialTransportCost = 0;
@@ -46,13 +81,19 @@ public class LabourAndMaterial extends Labour implements Transferable{
     }
     return materialTransportCost;
   }
-
+  /**
+  * @Override method to calculate total cost
+  * @return totalCost double
+  */
   @Override
   public double calcTotalCost(){
-    double totalCost = super.calcTotalCost() + (calcTransportationCost() + matertialTotalCost()) * SALES_TAX;
+    double totalCost = super.calcTotalCost() + (calcTransportationCost() + materialTotalCost()) * SALES_TAX;
     return totalCost;
   }  
-  
+  /**
+  * @Override toString()
+  * @return invoice breakdown
+  */    
   @Override
   public String toString(){
     String line = "";
