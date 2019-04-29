0019d5ae6c7247e9590d5874bd6603f2c051a5a2
bedtimebird <michaelbirdcet@gmail.com>
2019-04-28 20:51:24 -0700

    Create CustomerReport.java

---
+++ comp2613/lab2/io/CustomerReport.java
@@ -0,0 +1,19 @@
+/**
+ * Project: a00964363
+ * File: CustomerReport.java
+ * Date: Apr 27, 2019
+ * Time: 10:43:10 AM
+ */
+package lab2.io;
+
+/**
+ * @author Mike Bird, A00964363
+ *
+ */
+public class CustomerReport {
+
+	public CustomerReport() {
+
+	}
+
+}
