95007d1a2718f97132f7e95fec7bc805f123103b
bedtimebird <michaelbirdcet@gmail.com>
2019-04-28 20:53:49 -0700

    Create Validator.java

---
+++ comp2613/lab2/data/util/Validator.java
@@ -0,0 +1,15 @@
+/**
+ * Project: a00964363
+ * File: Validator.java
+ * Date: Apr 27, 2019
+ * Time: 10:43:34 AM
+ */
+package lab2.data.util;
+
+/**
+ * @author Mike Bird, A00964363
+ *
+ */
+public class Validator {
+
+}
