c1363a1a08bbc4f8160149727811df4d45630e6b
bedtimebird <michaelbirdcet@gmail.com>
2019-05-28 21:42:58 -0700

    Create Common.java

---
+++ comp2613/Assignment1/util/Common.java
@@ -0,0 +1,18 @@
+/**
+ * Project: Assignment1_start
+ * File: Common.java
+ * Date: May 27, 2019
+ * Time: 7:04:02 PM
+ */
+package a00964363.util;
+
+import java.time.format.DateTimeFormatter;
+
+/**
+ * @author Mike Bird, A00964363
+ *
+ */
+public class Common {
+
+	public static final DateTimeFormatter DATE_FORMAT = DateTimeFormatter.ofPattern("MMM dd yyyy");
+}
