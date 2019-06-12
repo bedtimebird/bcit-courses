18098d54346a14989e0b8997abf72f4a49dc3455
bedtimebird <michaelbirdcet@gmail.com>
2019-06-12 14:52:25 -0700

    Create Transferable.java

---
+++ comp2613/session8examples/bank/Transferable.java
@@ -0,0 +1,18 @@
+/**
+ * Project: examples8
+ * File: Transferable.java
+ * Date: Mar 3, 2015
+ * Time: 5:03:29 PM
+ */
+
+package bankdemo;
+
+/**
+ * @author Sam Cirka, A00123456
+ *
+ */
+public interface Transferable {
+
+	void transfer(int from, int to, double amount);
+	
+}
